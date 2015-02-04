3b20b2ee0c85ca7d3ae91b122895da3e53677ff5
mewmedia <jonnycook@jonnys-new-imac.home>
2015-02-04 16:11:12 +0000

    Skeleton application added from Netbeans to Git

--- public/js/forms/project/setup.js
+++ public/js/forms/project/setup.js
@@ -196,57 +196,24 @@ var Script = function () {
     });
     
     
-     $('#FormProjectSigned').on('submit', function(e) {
-        e.preventDefault();
-        e.stopPropagation();
-        
-        try {
-            var url = $(this).attr('action');
-            var params = 'ts='+Math.round(new Date().getTime()/1000);
-            $('#projectSignedLoader').fadeIn(function(){
-                $.ajax({
-                    type: 'POST',
-                    url: url,
-                    data: params, // Just send the Base64 content in POST body
-                    processData: false, // No need to process
-                    timeout: 60000, // 1 min timeout
-                    dataType: 'text', // Pure Base64 char data
-                    beforeSend: function onBeforeSend(xhr, settings) {},
-                    error: function onError(XMLHttpRequest, textStatus, errorThrown) {},
-                    success: function onUploadComplete(response) {
-                        //console.log(response); //return;
-                        try{
-                            var obj=jQuery.parseJSON(response);
-                            var k = 0;
-                            // an error has been detected
-                            if (obj.err == true) {
-                                
-                            } else{ // no errors
-                                document.location = obj.url;
-                            }
-                        }
-                        catch(error){
-                            
-                        }
-                    },
-                    complete: function(jqXHR, textStatus){
-                        $('#projectSignedLoader').fadeOut(function(){});
-                    }
-                });
-            });
-
-        } catch (ex) {
-
-        }/**/
-        return false;
-    });
-    
     
     $('#btn-confirm-project-signed').on('click', function(e) {
         e.preventDefault();
+        
         $('#FormProjectSigned').submit();
         return false;
     });
     
-
+    $('#btn-select-file').on('click',function(e) {
+        $('input[name=file]').trigger('click');
+    });
+    
+    $('input[name=file]').on('change',function(){
+        if ($(this).val().length) {
+            $('#lbl-file-info').text($(this).val().replace(/([^\\/]*[\\/])*([^.\\/]+([.][^.]+)+)$/i,'$2')).removeClass('label-important').addClass('label-success');
+        } else {
+            $('#lbl-file-info').text('No file selected').addClass('label-important').removeClass('label-success');
+        }
+    });
+    
 }();
