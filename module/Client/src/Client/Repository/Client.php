ab4b1a661130fd135d4534c96ed91bf7fccfb0b3
mewmedia <jonnycook@jonnys-new-imac.home>
2015-02-09 14:31:25 +0000

    Skeleton application added from Netbeans to Git

--- module/Client/src/Client/Repository/Client.php
+++ module/Client/src/Client/Repository/Client.php
@@ -10,7 +10,7 @@ class Client extends EntityRepository
     public function findByName($name) {
         // First get the EM handle
         // and call the query builder on it
-        $query = $this->_em->createQuery("SELECT c FROM Client\Entity\Client c WHERE c.name=".$name."");
+        $query = $this->_em->createQuery("SELECT c FROM Client\Entity\Client c WHERE c.name='{$name}'");
         return $query->getResult();
     }
 
